6b00758b139b5904a85e1796ddb543eec5f83bd9
Valen <valenreynoso17@gmail.com>
2022-09-29 10:35:56 -0300

    Release-bugfix-1.1.0

--- app/src/main/java/com/example/tp_ingsw/FirstFragment.java
+++ app/src/main/java/com/example/tp_ingsw/FirstFragment.java
@@ -16,6 +16,7 @@ public class FirstFragment extends Fragment {
     private FragmentFirstBinding binding;
 
     // comentario para inciso f
+    // comentario para Release 1.1.0
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
